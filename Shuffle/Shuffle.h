f7ee32ebb415e7762ae17a71ae126cd5cd5c76e5
Veera Gutti <veera@bytemark.co>
2021-10-19 18:01:21 +0530

    Initial Commit

---
+++ Shuffle/Shuffle.h
@@ -0,0 +1,18 @@
+//
+//  Shuffle.h
+//  Shuffle
+//
+//  Created by VEERA NARAYNA GUTTI on 19/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shuffle.
+FOUNDATION_EXPORT double ShuffleVersionNumber;
+
+//! Project version string for Shuffle.
+FOUNDATION_EXPORT const unsigned char ShuffleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shuffle/PublicHeader.h>
+
+
